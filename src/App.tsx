dea43e9d3fed1f895dec3824a741580aa500be9c
umerbaig123 <=>
2024-08-12 16:36:16 -0700

    adds intro section

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,20 @@ function App() {
   return (
     <>
       <NavBar />
+      <div className="intro">
+        <div className="heading">Be the market leader in your industry</div>
+        <div className="sub-heading">
+          Access 2,000+ passionate and experienced software engineers with
+          skillsets in 100+ technologies, in your timezone.
+        </div>
+        <div style={{ display: "flex", gap: 10 }}>
+          <button className="btn schedule">Schedule Free Strategy Call</button>
+          <button className="btn">See our Work</button>
+        </div>
+      </div>
+      <div className="section-heading">
+        From Idea to Completion, we bring Full-Stack Expertise
+      </div>
     </>
   );
 }
